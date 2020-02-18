3588143e76dafa6bc59d9e2f4e84037ea313ea03
HKUMAS <hsnkms75@gmail.com>
2020-02-18 16:23:34 +0100

    some comments

--- src/ApplicationParty.java
+++ src/ApplicationParty.java
@@ -1,20 +1,25 @@
 public class ApplicationParty {
     public static void main(String[] args) {
+        //creating parties
         Party party1 = new Party("Amsterdam", "birthday");
         Party party2 = new Party("Amsterdam", "new year");
 
+        //creating people
         Person person1 = new Person("Hasan", 45, "wine");
         Person person2 = new Person("John", 34, "beer");
 
+        //joining and leaving party
         party1.enter(person1);
         party1.enter(person2);
         party1.leave(person2);
         party1.leave(person2);
         party1.enter(person2);
 
+        //showing the number of people and attendees in a party
         party1.numberOfPeople();
         party1.attendeesOfParty();
 
+        //joining and leaving party
         party2.enter(person1);
         party2.enter(person2);
         party2.enter(person2);
@@ -22,6 +27,7 @@ public class ApplicationParty {
         party2.leave(person2);
         party2.enter(person2);
 
+        //showing the number of people and attendees in a party
         party2.numberOfPeople();
         party2.attendeesOfParty();
     }
--- src/Party.java
+++ src/Party.java
@@ -10,32 +10,33 @@ public class Party {
         this.location = location;
         this.name = name;
     }
-
+    //this function adds a person to the party
     public void enter(Person person){
         if(person.isAttendingParty()){
             System.out.println("Person " + person.getName() + " is already at the party " + person.getCurrentPartyName());
         }else {
             person.setAttendingParty(true);
             person.setCurrentPartyName(name);
-            System.out.println("Person " + person.getName() + " entered party " + name);
+            System.out.println("Person " + person.getName() + " entered party " + name + " in " + location);
             personList.add(person);
         }
     }
-
+    //this function removes a person from the party
     public void leave(Person person){
         if (personList.contains(person)) {
             personList.remove(person);
-            System.out.println("Person " + person.getName() + " left party " + name);
+            System.out.println("Person " + person.getName() + " left party " + name + " in " + location);
             person.setAttendingParty(false);
             person.setCurrentPartyName(" ");
         }else {
             System.out.println("Person " + person.getName() + " is not at this party right now ");
         }
     }
-
+    //this function calculates and print the number of people attending the party
     public void numberOfPeople(){
-        System.out.println("There are currently " + personList.size() + " people on the party "+ name);
+        System.out.println("There are currently " + personList.size() + " people on the party "+ name + " in " + location);
     }
+    //this function prints the information of the attendees
     public void attendeesOfParty(){
         for (Person person: personList) {
             System.out.println(person);
